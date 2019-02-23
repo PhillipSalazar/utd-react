46ba39145ed4bd953bfc3a4602da6cf0fb3d666d
Limeapple <Kevinliu54321@gmail.com>
2019-02-23 16:39:31 -0600

    chat

--- src/App.js
+++ src/App.js
@@ -8,20 +8,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-            <FontAwesomeIcon icon={faCoffee} />
-        </header>
+
+             
          <div>
         <div  id="t1">
           <div  id="t2">
@@ -29,12 +17,13 @@ class App extends Component {
               <div id="t4">
                 <section>
                   <ul>
-                    <a href="#t2"> <FontAwesomeIcon icon={faCoffee} /></a>
-                     <a href="#t3"> <FontAwesomeIcon icon={faCoffee} /></a>
-                    <a href="#t4"> <FontAwesomeIcon icon={faCoffee} /></a>
+                    <a href="#t1"> <FontAwesomeIcon icon={faCoffee} id="uno"/></a>
+                    <a href="#t2"> <FontAwesomeIcon icon={faCoffee} id="dos"/></a>
+                     <a href="#t3"> <FontAwesomeIcon icon={faCoffee} id="tres"/></a>
+                    <a href="#t4"> <FontAwesomeIcon icon={faCoffee} id="cuatro"/></a>
                   </ul>
                   <div className="page" id="p1">
-                    <li className="icon fa fa-home"><span className="title">Home</span><span class="hint">Like this pen to see the magic!...<br /> Just kidding, it won't happen anything but I'll be really happy If you do so.</span></li>
+                    <span className="title">Home</span><span class="hint">Like this pen to see the magic!...<br /> Just kidding, it won't happen anything but I'll be really happy If you do so.</span></li>
                   </div>
                   <div className="page" id="p2">
                     <li className="icon fa fa-keyboard-o"><span className="title">Type</span></li>
@@ -42,9 +31,12 @@ class App extends Component {
                   <div className="page" id="p3">
                     <li className="icon fa fa-coffee"><span className="title">Coffee</span></li>
                   </div>
-                   
+         <div className="page" id="p4">
+                    <li className="icon fa fa-coffee"><span className="title">Coffee</span></li>
+                  </div>
+
 </section>
-       
+
      </div>
               </div>
             </div>
